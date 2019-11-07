ba44d0043c22217e88220eb869b255483fa49554
cavu757 <jose.pehovazdiez@gmail.com>
2019-11-06 21:33:37 -0500

    update meal model for serializer

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,8 +1,10 @@
 class Meal < ApplicationRecord
-  belongs_to :user
   belongs_to :food
+  belongs_to :user
   has_many :comments
 
   scope :my_meal, -> (cook_id){where(cook_id: cook_id)}
+
+
 
 end
