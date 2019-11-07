c2ae9b768e96a6081aa54388cd35203318171f26
cavu757 <jose.pehovazdiez@gmail.com>
2019-11-06 21:32:57 -0500

    Update user_serializer.rb

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :is_cook
+  attributes :id, :name
   has_many :meals
-  has_many :foods, through: :meals
-  has_many :comments, through: :meals 
 end
