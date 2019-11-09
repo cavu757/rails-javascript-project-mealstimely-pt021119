0ea1f7373bfc8e4e224100f67124babdcf3b66d3
cavu757 <jose.pehovazdiez@gmail.com>
2019-11-09 16:42:39 -0500

    Updated user JS file to listen and format
    added to listen for the comment button click and also for format of the date

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,14 +2,17 @@
 
 $(document).on('turbolinks:load', function(){
   listenMealsIHaveCreatedButtonClick();
+  listenCommentButtonClick();
+
 });
 
 
 function listenMealsIHaveCreatedButtonClick() {
   $('#food-created').val('');
+
   $('#meals-i-have-created').on('click', function(event) {
     event.preventDefault();
-    $('#food-created').empty()
+
     let id = $('#meals-i-have-created').attr("data")
     $.get("/users/"+id+"/meals.json", function(json){
       if (json.length == 0){alert("You have no meals, click on food available and create some meals")}
@@ -20,11 +23,11 @@ function listenMealsIHaveCreatedButtonClick() {
 
 function getUserMeals(json){
   let meals = json
-
+  $('#food-created').empty();
   meals.forEach(function(meal){
 
     $('#food-created').append(
-    '<div class="index-box">' + '<h3>' + meal.meal_name + '</h3>' + '<img src="' + meal.food.picture + '" width="75%">' + '<p>Cooked by: ' + meal.food.cook.name  + '</p>' + '</div>');
+    '<div class="index-box">' + '<h5>' + meal.created_at + '</h5><h3>' + meal.meal_name + '</h3>' + '<a href="' + meal.user.id + '/meals/' + meal.id + '"><img src="' + meal.food.picture + '" width="75%"></a>' + '<h5>Cooked by: ' + meal.food.cook.name  + '</h5></div>');
   });
 
 }
