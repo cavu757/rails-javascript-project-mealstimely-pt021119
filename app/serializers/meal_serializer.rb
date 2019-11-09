459d270c0956c9f548d5c06dcaa8c0e36e5d596a
cavu757 <jose.pehovazdiez@gmail.com>
2019-11-09 16:39:27 -0500

    Update serializer with created date
    insert created date for JS comment constructor

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,6 +1,10 @@
 class MealSerializer < ActiveModel::Serializer
-  attributes :id, :meal_name
+  attributes :id, :meal_name, :created_at
   belongs_to :user
   belongs_to :food
   has_many :comments
+
+  def created_at
+    date = object.created_at.strftime("%d %b %Y") 
+  end
 end
