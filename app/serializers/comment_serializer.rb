b2ac2375911ab4746b76b6d698b4eb5bc010b9f9
cavu757 <jose.pehovazdiez@gmail.com>
2019-11-09 16:39:51 -0500

    added created date for comment
    added created date to serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,10 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content
+  attributes :id, :content, :created_at
   belongs_to :user
   belongs_to :meal
+
+  def created_at
+    date = object.created_at.strftime("%d %b %Y at%l:%M %p")
+  end
+
 end
