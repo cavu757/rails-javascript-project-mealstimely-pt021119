518695d449d80d8e5b59e83727635f437244af65
cavu757 <jose.pehovazdiez@gmail.com>
2019-11-09 17:52:45 -0500

    updated meals.js to replace no comment comments

--- app/assets/javascripts/meals.js
+++ app/assets/javascripts/meals.js
@@ -1,9 +1,16 @@
 
 function listenCommentButtonClick(){
+
   // $("#print_comments").hide()
   $("#new_comment").on("submit", function(event){
 
     event.preventDefault();
+
+    let idMeal = $("#comment_meal_id").val()
+    $.get("/meals/"+idMeal+"/comments.json", function(json){
+    if (json.length < 1){
+      $("#no_meal_comments").replaceWith('')
+    }})
 
     let values = $(this).serialize();
     let attributes = $.post('/comments', values);
@@ -43,6 +50,7 @@ function Comment(attr){
 }
 
 Comment.prototype.addCommentPrototype = function(){
+
   let html = '<p>On ' + this.time + ', ' + this.user + ' said: "' + this.content + '"</p>'
   $("#print_comments").prepend(html)
 }
